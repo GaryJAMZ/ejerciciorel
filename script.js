1f6d359513d9c7626f6cdd8d5251a6c423b2c005
Antonio Garibay <GaribayJAMZXN@outlook.com>
2022-04-18 13:32:15 -0500

    relog concluido

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ setInterval(() =>{
     let segundos = document.getElementById('segundos');
     let ampm = document.getElementById('ampm');
 
+    let hor = document.getElementById('hor');
+    let min = document.getElementById('min');
+    let seg = document.getElementById('seg');
+
+    let hor_punto = document.querySelector('.hor_punto');
+    let min_punto = document.querySelector('.min_punto');
+    let seg_punto = document.querySelector('.seg_punto');
+
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
@@ -17,8 +25,16 @@ setInterval(() =>{
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
 
-    horas.innerHTML = h;
-    minutos.innerHTML = m;
-    segundos.innerHTML = s;
+    horas.innerHTML = h + "<br><span>Horas</span>";
+    minutos.innerHTML = m + "<br><span>Minutos</span>";
+    segundos.innerHTML = s + "<br><span>Segundos</span>";
     ampm.innerHTML = am;
+
+    hor.style.strokeDashoffset = 440 - (440 * h) / 12;
+    min.style.strokeDashoffset = 440 - (440 * m) / 60;
+    seg.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+    hor_punto.style.transform = `rotate(${h * 30}deg)`;
+    min_punto.style.transform = `rotate(${m * 6}deg)`;
+    seg_punto.style.transform = `rotate(${s * 6}deg)`;
 })
